389c7cb876cfc58d3a6e505bcf2956f31dc7e693
佐々木悠介 <sasaru1027@gmail.com>
2021-05-01 17:38:23 +0900

    netkeibaへのログイン情報を環境変数に埋め込む#14

--- lib/utils/netkeiba_login.py
+++ lib/utils/netkeiba_login.py
@@ -1,12 +1,17 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+# https://pypi.org/project/python-dotenv/
+load_dotenv()
 
 def login_session():
   session = requests.Session()
   login_data = {
     'pid':'login',
     'action':'auth',
-    'login_id':'LOGINUSER',
-    'pswd':'PASSWORD'
+    'login_id':os.getenv('NETKEIBA_LOGIN_ID'),
+    'pswd':os.getenv('NETKEIBA_PWD')
   }
 
   session.post('https://regist.netkeiba.com/account/', data=login_data)
